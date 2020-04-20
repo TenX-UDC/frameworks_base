63d16fc931d99a8aafa18d76089313d0a20b9e8c
Linux4 <tim@linux4.de>
2020-04-20 17:57:43 +0200

    SystemUI: Add powershare QS tile
    Author: Linux4 <tim@linux4.de>
    Date:   Mon Apr 20 17:57:43 2020 +0200
    
        base: SystemUI: add qs powershare tile
    
        Change-Id: Ibcf5cc3e766d971d9dca9242bf2770093d4d3099
    
    Author: Linux4 <tim@linux4.de>
    Date:   Sat May 2 12:54:49 2020 +0200
    
        base: SystemUI: fix NPE in PowerShareTile
    
        Change-Id: If69846f3e3d4f3a12c0f78f47b5adc1008888de1
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Tue Jul 7 13:27:12 2020 +0200
    
        SystemUI: Call setOnlyAlertOnce(true) for PowerShare notification
    
        * Otherwise we end up getting notification sound
          every 20 seconds when using it...
    
        Change-Id: Ia56b01f332860b64f0d2c532884e6bcaa2acfb17
    
    Change-Id: I4042a2cb04ee05a41993ed24cd64fd509a04673f
    
    Author: cjh1249131356 <cjh1249131356@gmail.com>
    Date:   Mon Jan 24 20:37:11 2022 +0800
    
        SystemUI: Improve Powershare QS tile
    
        * Grey out while device is charging.
        * Nuke unnecessary label changes as they are invisible in a12 qs panel.
    
        Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    
    Author: Henrique Silva <jhsv09@gmail.com>
    Date:   Sun May 15 16:46:53 2022 -0300
    
        SystemUI: Improve powershare strings
    
    Author: aswin7469 <aswinas@pixysos.com>
    Date:   Tue Nov 8 19:30:53 2022 +0530
    
        SystemUI: update powershare QS tile icon
    
        * kanged from nothingos and modified
    
        Change-Id: Ia3928d4c7ff8ec99c59af5873ad6ae659614026b
        Signed-off-by: aswin7469 <aswinas@pixysos.com>
    
    Change-Id: I4042a2cb04ee05a41993ed24cd64fd509a04673f
    
    Change-Id: Ia09a674745adb433f3aaeebfeb79899906350793

--- packages/SystemUI/src/com/android/systemui/tenx/TenXModule.kt
+++ packages/SystemUI/src/com/android/systemui/tenx/TenXModule.kt
@@ -10,6 +10,7 @@ import com.android.systemui.qs.tiles.AmbientDisplayTile
 import com.android.systemui.qs.tiles.AODTile
 import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.HeadsUpTile
+import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.SyncTile
 
 import dagger.Binds
@@ -42,6 +43,12 @@ interface TenXModule {
     @IntoMap
     @StringKey(HeadsUpTile.TILE_SPEC)
     fun bindHeadsUpTile(headsUpTile: HeadsUpTile): QSTileImpl<*>
+
+    /** Inject PowerShareTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(PowerShareTile.TILE_SPEC)
+    fun bindPowerShareTile(powerShareTile: PowerShareTile): QSTileImpl<*>
 
     /** Inject SyncTile into tileMap in QSModule */
     @Binds
