0187ca91a882adbb6cd0167a6f978979251d8dbb
Pranav Vashi <neobuddy89@gmail.com>
2023-11-27 12:41:12 +0530

    SystemUI: Add separate customization for QS footer transparency
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6051,6 +6051,11 @@ public final class Settings {
         public static final String QS_PANEL_STYLE = "qs_panel_style";
 
         /**
+         * @hide
+         */
+        public static final String QS_FOOTER_TRANSPARENCY = "qs_footer_transparency";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
