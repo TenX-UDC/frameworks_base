AOSPA/android_frameworks_base-old

commit 1d384463675effe0544a38ec137fa5021d571fc2

Cache pin algorithms in PinSet
Change-Id: I6c2cd0261885b656910feffe76d67e169d8523c8
Signed-off-by: Pranav <npv12@iitbbs.ac.in>

core/java/android/security/net/config/PinSet.java
@@ -22,6 +22,7 @@ import java.util.Set;
 
 /** @hide */
 public final class PinSet {
+    private static Set<String> algorithms;
     public static final PinSet EMPTY_PINSET =
             new PinSet(Collections.<Pin>emptySet(), Long.MAX_VALUE);
     public final long expirationTime;
@@ -36,10 +37,11 @@ public final class PinSet {
     }
 
     Set<String> getPinAlgorithms() {
-        // TODO: Cache this.
-        Set<String> algorithms = new ArraySet<String>();
-        for (Pin pin : pins) {
-            algorithms.add(pin.digestAlgorithm);
+        if(algorithms == null){
+            algorithms = new ArraySet<String>();
+            for (Pin pin : pins) {
+                algorithms.add(pin.digestAlgorithm);
+            }
         }
         return algorithms;
     }

TenX-UDC/frameworks_base

commit a9780f68b84250de42bae81804eadbec0824edec

base: Fixup and improve algorithms caching
* algorithms shouldn't be a static field since pins is an instance variable
* use stream API's to map and collect pin algorithms to an ArraySet

Fixes: 1d384463675effe0544a38ec137fa5021d571fc2 ("Cache pin algorithms in PinSet")
Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
Change-Id: I8a0cd85a350901b9900044cb536bd32b7cb40e02

core/java/android/security/net/config/PinSet.java
@@ -17,16 +17,18 @@
 package android.security.net.config;
 
 import android.util.ArraySet;
+
 import java.util.Collections;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /** @hide */
 public final class PinSet {
-    private static Set<String> algorithms;
     public static final PinSet EMPTY_PINSET =
             new PinSet(Collections.<Pin>emptySet(), Long.MAX_VALUE);
     public final long expirationTime;
     public final Set<Pin> pins;
+    private final Set<String> algorithms;
 
     public PinSet(Set<Pin> pins, long expirationTime) {
         if (pins == null) {
@@ -34,15 +36,12 @@ public final class PinSet {
         }
         this.pins = pins;
         this.expirationTime = expirationTime;
+        this.algorithms = pins.stream()
+            .map(pin -> pin.digestAlgorithm)
+            .collect(Collectors.toCollection(ArraySet::new));
     }
 
     Set<String> getPinAlgorithms() {
-        if(algorithms == null){
-            algorithms = new ArraySet<String>();
-            for (Pin pin : pins) {
-                algorithms.add(pin.digestAlgorithm);
-            }
-        }
         return algorithms;
     }
 }