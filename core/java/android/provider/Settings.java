b6b3b20eaa85d2df5471328e5556128023abc25a
maxwen <max.weninger@gmail.com>
2024-09-17 13:31:07 +0530

    SystemUI: [SQUASH] Keyguard battery bar [1/2]
    Squashed-in:
    * SystemUI: keyguard battery bar in aod on charging [1/2]
      or always with an switch (maxwen)
    [micky387] Remove tuner dependency and add settings for our OmniGears
    
    [spezi77] Updated for A13 & A14
    * SystemUI: Also show the battery bar if the battery is not charging.
      Only show it while in doze mode (Ambient/AoD) (spezi77)
    
    [Albinoman887] Fix USB plug/unplug on AOD when battery bar setting is disabled
    * This check was added after the following commit that intruduces the Keyguard Battery Bar
      Setting was authored. Simply bring back the check and bail out of the function if it isn't
      visible.
    
    Co-authored-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7505,6 +7505,16 @@ public final class Settings {
         public static final String LOCKSCREEN_MAX_NOTIF_CONFIG = "lockscreen_max_notif_cofig";
 
         /**
+         * @hide
+         */
+        public static final String CUSTOM_KEYGUARD_SHOW_BATTERY_BAR = "sysui_keyguard_show_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String CUSTOM_KEYGUARD_SHOW_BATTERY_BAR_ALWAYS = "sysui_keyguard_show_battery_bar_always";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
